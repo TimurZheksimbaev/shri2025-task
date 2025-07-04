e08aad999feb4b7cd1f867abfce576201f1c94a3
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-07-05 02:07:28 +0300

    fixed visual mistakes with footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
-import { Footer } from './components/Footer';
 
 export function App() {
   return (
     <>
       <Header />
       <Main />
-      <Footer />
     </>
   );
 }
